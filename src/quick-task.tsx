999761d3c703246e452b0c3da25f694b28052f96
Weber Grandizoli <webergnr@gmail.com>
2023-07-05 13:39:34 -0300

    WIP

--- src/quick-task.tsx
+++ src/quick-task.tsx
@@ -1,90 +1,152 @@
-import { popToRoot, showToast, Toast } from "@raycast/api";
-import { addDays, addMinutes, endOfDay } from "date-fns";
-import { axiosPromiseData, fetcher } from "./utils/axiosPromise";
-import { formatDuration, parseDurationToMinutes, TIME_BLOCK_IN_MINUTES } from "./utils/dates";
-import { ApiResponseUser } from "./hooks/useUser.types";
+// import { popToRoot, showToast, Toast } from "@raycast/api";
+// import { addDays, addMinutes, endOfDay } from "date-fns";
+// import { axiosPromiseData, fetcher } from "./utils/axiosPromise";
+// import { formatDuration, parseDurationToMinutes, TIME_BLOCK_IN_MINUTES } from "./utils/dates";
+// import { ApiResponseUser } from "./hooks/useUser.types";
 
-type Props = { arguments: { event: string; time: string } };
+// type Props = { arguments: { event: string; time: string } };
 
-export default async function Command(props: Props) {
-  const { event, time } = props.arguments;
+// export default async function Command(props: Props) {
+//   const { event, time } = props.arguments;
 
-  if (!event || !time) {
-    return;
-  }
+//   if (!event || !time) {
+//     return;
+//   }
 
-  const toast = await showToast({
-    style: Toast.Style.Animated,
-    title: "Creating task...",
-  });
+//   const toast = await showToast({
+//     style: Toast.Style.Animated,
+//     title: "Creating task...",
+//   });
 
-  try {
-    if (Number(parseDurationToMinutes(time)) % 15 !== 0) {
-      toast.style = Toast.Style.Failure;
-      toast.title = "Time must be in a interval of 15 minutes. (15/30/45/60...)";
-      return;
-    }
+//   try {
+//     if (Number(parseDurationToMinutes(time)) % 15 !== 0) {
+//       toast.style = Toast.Style.Failure;
+//       toast.title = "Time must be in a interval of 15 minutes. (15/30/45/60...)";
+//       return;
+//     }
 
-    if (time.replace(/(\s|^)\d+(\s|$)/g, "") === "") {
-      toast.style = Toast.Style.Failure;
-      toast.title = "Please provide a valid time. (hours/min)";
-      return;
-    }
+//     if (time.replace(/(\s|^)\d+(\s|$)/g, "") === "") {
+//       toast.style = Toast.Style.Failure;
+//       toast.title = "Please provide a valid time. (hours/min)";
+//       return;
+//     }
 
-    const durationBlock = Number(parseDurationToMinutes(formatDuration(time))) / TIME_BLOCK_IN_MINUTES;
+//     const durationBlock = Number(parseDurationToMinutes(formatDuration(time))) / TIME_BLOCK_IN_MINUTES;
 
-    const [user, userError] = await axiosPromiseData<ApiResponseUser>(fetcher("/users/current"));
+//     const [user, userError] = await axiosPromiseData<ApiResponseUser>(fetcher("/users/current"));
 
-    if (!user && userError) {
-      toast.style = Toast.Style.Failure;
-      toast.title = "Failed to get user info. Please check your settings.";
-      return;
-    }
+//     if (!user && userError) {
+//       toast.style = Toast.Style.Failure;
+//       toast.title = "Failed to get user info. Please check your settings.";
+//       return;
+//     }
 
-    const userDefaults = {
-      defaultDueDate: addDays(new Date(), user?.features.taskSettings.defaults.dueInDays || 0),
-      defaultSnoozeDate: addMinutes(new Date(), user?.features.taskSettings.defaults.delayedStartInMinutes || 0),
-      minDuration: (user?.features.taskSettings.defaults.minChunkSize || 1) * TIME_BLOCK_IN_MINUTES,
-      maxDuration: (user?.features.taskSettings.defaults.maxChunkSize || 1) * TIME_BLOCK_IN_MINUTES,
-      duration: (user?.features.taskSettings.defaults.timeChunksRequired || 1) * TIME_BLOCK_IN_MINUTES,
-      category: user?.features.taskSettings.defaults.category || "WORK",
-      private: user?.features.taskSettings.defaults.alwaysPrivate || true,
-    };
+//     const userDefaults = {
+//       defaultDueDate: addDays(new Date(), user?.features.taskSettings.defaults.dueInDays || 0),
+//       defaultSnoozeDate: addMinutes(new Date(), user?.features.taskSettings.defaults.delayedStartInMinutes || 0),
+//       minDuration: (user?.features.taskSettings.defaults.minChunkSize || 1) * TIME_BLOCK_IN_MINUTES,
+//       maxDuration: (user?.features.taskSettings.defaults.maxChunkSize || 1) * TIME_BLOCK_IN_MINUTES,
+//       duration: (user?.features.taskSettings.defaults.timeChunksRequired || 1) * TIME_BLOCK_IN_MINUTES,
+//       category: user?.features.taskSettings.defaults.category || "WORK",
+//       private: user?.features.taskSettings.defaults.alwaysPrivate || true,
+//     };
 
-    const data = {
-      title: event,
-      eventCategory: userDefaults.category,
-      timeChunksRequired: durationBlock,
-      snoozeUntil: userDefaults.defaultSnoozeDate.toJSON(),
-      due: userDefaults.defaultDueDate.toJSON(),
-      minChunkSize: userDefaults.minDuration,
-      maxChunkSize: userDefaults.maxDuration,
-      alwaysPrivate: userDefaults.private,
-    };
+//     const data = {
+//       title: event,
+//       eventCategory: userDefaults.category,
+//       timeChunksRequired: durationBlock,
+//       snoozeUntil: userDefaults.defaultSnoozeDate.toJSON(),
+//       due: userDefaults.defaultDueDate.toJSON(),
+//       minChunkSize: userDefaults.minDuration,
+//       maxChunkSize: userDefaults.maxDuration,
+//       alwaysPrivate: userDefaults.private,
+//     };
 
-    console.log("### => [POST] /tasks", data);
+//     console.log("### => [POST] /tasks", data);
 
-    const [task, error] = await axiosPromiseData(
-      fetcher("/tasks", {
-        method: "POST",
-        data,
-      })
-    );
+//     const [task, error] = await axiosPromiseData(
+//       fetcher("/tasks", {
+//         method: "POST",
+//         data,
+//       })
+//     );
 
-    console.log("### => [RESPONSE] /tasks", task);
+//     console.log("### => [RESPONSE] /tasks", task);
 
-    if (!task && error) throw error;
+//     if (!task && error) throw error;
 
-    toast.style = Toast.Style.Success;
-    toast.title = "Task created!";
-    popToRoot();
-  } catch (err) {
-    console.error("Error while creating task", err);
+//     toast.style = Toast.Style.Success;
+//     toast.title = "Task created!";
+//     popToRoot();
+//   } catch (err) {
+//     console.error("Error while creating task", err);
 
-    toast.style = Toast.Style.Failure;
-    toast.title = "Failed to create task";
-    if (err instanceof Error) {
-      toast.message = err.message;
-    }
-  }
+//     toast.style = Toast.Style.Failure;
+//     toast.title = "Failed to create task";
+//     if (err instanceof Error) {
+//       toast.message = err.message;
+//     }
+//   }
+// }
+
+import { Action, ActionPanel, Icon, List, useNavigation } from "@raycast/api";
+import { useState } from "react";
+import { setTimeout } from "timers/promises";
+import { FilteredAnything } from "./filtered-anything";
+
+export type ListType = {
+  title: string;
+  type: "task" | "scheduling-link";
+};
+
+export default function Command() {
+  const { push } = useNavigation();
+  const [list, setList] = useState<ListType[]>([]);
+  const [query, setQuery] = useState<string>("");
+
+  const delayedInput = async (input: string) => {
+    setQuery(input);
+    if (list.length !== 0) return;
+    await setTimeout(2000);
+    setList([
+      {
+        title: "Ask anything about Tasks",
+        type: "task",
+      },
+      {
+        title: "Ask anything about Scheduling Links",
+        type: "scheduling-link",
+      },
+    ]);
+  };
+
+  return (
+    <List searchBarPlaceholder="Ask anything..." onSearchTextChange={delayedInput}>
+      {list.length === 0 ? (
+        <List.EmptyView
+          icon={Icon.LightBulb}
+          description={`“Some copy about the task creation ”`}
+          title="Ask Reclaim to create a task"
+        />
+      ) : (
+        list.map((item) => (
+          <List.Item
+            key={item.title}
+            title={item.title}
+            icon={Icon.LightBulb}
+            actions={
+              <ActionPanel>
+                <Action
+                  title={item.title}
+                  onAction={() => {
+                    push(<FilteredAnything type={item.type} query={query} />);
+                  }}
+                />
+              </ActionPanel>
+            }
+          />
+        ))
+      )}
+    </List>
+  );
 }
