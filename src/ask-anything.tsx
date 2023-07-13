a706aac88c4e9112368043fdaed1b76c6e219a63
Weber Grandizoli <webergnr@gmail.com>
2023-07-13 15:54:33 -0300

    Revert "Remove ask anything files (#36)"
    This reverts commit 867af0ec4ae95adc1eaefafa35da95e95c663ad5.

---
+++ src/ask-anything.tsx
@@ -0,0 +1,63 @@
+import { Action, ActionPanel, Icon, List, useNavigation } from "@raycast/api";
+import { useState } from "react";
+import { setTimeout } from "timers/promises";
+import { FilteredAnything } from "./filtered-anything";
+
+export type ListType = {
+  title: string;
+  type: "task" | "scheduling-link";
+};
+
+export default function Command() {
+  const { push } = useNavigation();
+  const [list, setList] = useState<ListType[]>([]);
+  const [query, setQuery] = useState<string>("");
+
+  const delayedInput = async (input: string) => {
+    setQuery(input);
+    if (list.length !== 0) return;
+    await setTimeout(2000);
+    setList([
+      {
+        title: "Ask anything about Tasks",
+        type: "task",
+      },
+      {
+        title: "Ask anything about Scheduling Links",
+        type: "scheduling-link",
+      },
+    ]);
+  };
+
+  return (
+    <List searchBarPlaceholder="Ask anything..." onSearchTextChange={delayedInput}>
+      {list.length === 0 ? (
+        <List.EmptyView
+          icon={Icon.LightBulb}
+          description={`“Get me 2h for meeting prep in the next couple weeks”
+“Make sure I exercise 3x this week in the afternoons”
+“Clear my day”`}
+          title="Ask Reclaim to do anything"
+        />
+      ) : (
+        list.map((item) => (
+          <List.Item
+            key={item.title}
+            title={item.title}
+            icon={Icon.LightBulb}
+            actions={
+              <ActionPanel>
+                <Action
+                  title={item.title}
+                  onAction={() => {
+                    push(<FilteredAnything type={item.type} query={query} />);
+                  }}
+                />
+              </ActionPanel>
+            }
+          />
+        ))
+      )}
+    </List>
+  );
+}
